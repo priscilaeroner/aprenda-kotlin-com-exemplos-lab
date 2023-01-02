59838de6716f964fa4a4289231ef867a4af95f5b
Priscila Robles Pivatto <119367886+priscilaeroner@users.noreply.github.com>
2023-01-02 20:00:57 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,53 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+open class Aluno(val name: String) {
+    fun infos() {
+        println("$name, Seja bem vindo!")
     }
 }
 
+class Usuario(name: String) : Aluno(name = name)
+
+
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60) {
+
+    fun imprimir(): String {
+        return "Curso $nome com duração : $duracao "
+    }
+}
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+
+    val inscritos = mutableListOf<Aluno>()
+
+    fun matricular(usuario: Aluno) {
+        inscritos.add(usuario)
+    }
+
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val maria:Aluno = Usuario("Maria")
+    println(maria.infos())
+    
+    val joao:Aluno = Usuario("João")
+    println(joao.infos())
+
+    val listaCursos = listOf(
+        ConteudoEducacional("Kotlin"),
+        ConteudoEducacional("Java")
+    )
+
+    var formacaoR = Formacao(Nivel.BASICO.toString(), listaCursos)
+
+    formacaoR.matricular(maria)
+    formacaoR.matricular(joao)
+    
+    println("Cursos de formação ${formacaoR.nome}")
+    listaCursos.forEach { println(it.imprimir())
+    
+    formacaoR.inscritos.forEach { println("Inscritos: ${it.name}") }
+    formacaoR.conteudos.forEach { println("Cursos: ${it.nome} - Duração: ${it.duracao}") }
+
+    }
 }
